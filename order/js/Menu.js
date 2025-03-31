48e3eecf5781fdffb61112fc951c5f626f8b35da
lamaiwa1992 <77945507+lamaiwa1992@users.noreply.github.com>
2025-03-31 22:55:29 +0900

    Update Menu.js

--- order/js/Menu.js
+++ order/js/Menu.js
@@ -1,28 +1,28 @@
 const items = {
     coffee: [
-        { id: 1, name: "Cà Phê Sữa", price: 50000, image: "images/1.png" },
-        { id: 2, name: "Cà Phê Đen", price: 40000, image: "images/2.png" },
-        { id: 3, name: "Cà Phê Mocha", price: 55000, image: "images/3.png" },
-        { id: 4, name: "Cà Phê Americano", price: 45000, image: "images/4.png" },
-        { id: 5, name: "Cà Phê Espresso", price: 40000, image: "images/5.png" },
-        { id: 6, name: "Cà Phê Latte", price: 50000, image: "images/6.png" },
-        { id: 7, name: "Cà Phê Cappuccino", price: 52000, image: "images/7.png" },
-        { id: 8, name: "Cà Phê Irish", price: 60000, image: "images/8.png" },
-        { id: 9, name: "Cà Phê Dalgona", price: 65000, image: "images/9.png" },
-        { id: 10, name: "Cà Phê Việt Nam", price: 45000, image: "images/10.png" },
-        { id: 11, name: "Cà Phê Cold Brew", price: 70000, image: "images/11.png" },
-        { id: 12, name: "Cà Phê Caramel", price: 55000, image: "images/12.png" }
+        { id: 1, name: "Cà Phê Sữa", price: 50000, image: "bomilktea.store/order/images/1.png" },
+        { id: 2, name: "Cà Phê Đen", price: 40000, image: "bomilktea.store/order/images/2.png" },
+        { id: 3, name: "Cà Phê Mocha", price: 55000, image: "bomilktea.store/order/images/3.png" },
+        { id: 4, name: "Cà Phê Americano", price: 45000, image: "bomilktea.store/order/images/4.png" },
+        { id: 5, name: "Cà Phê Espresso", price: 40000, image: "bomilktea.store/order/images/5.png" },
+        { id: 6, name: "Cà Phê Latte", price: 50000, image: "bomilktea.store/order/images/6.png" },
+        { id: 7, name: "Cà Phê Cappuccino", price: 52000, image: "bomilktea.store/order/images/7.png" },
+        { id: 8, name: "Cà Phê Irish", price: 60000, image: "bomilktea.store/order/images/8.png" },
+        { id: 9, name: "Cà Phê Dalgona", price: 65000, image: "bomilktea.store/order/images/9.png" },
+        { id: 10, name: "Cà Phê Việt Nam", price: 45000, image: "bomilktea.store/order/images/10.png" },
+        { id: 11, name: "Cà Phê Cold Brew", price: 70000, image: "bomilktea.store/order/images/11.png" },
+        { id: 12, name: "Cà Phê Caramel", price: 55000, image: "bomilktea.store/order/images/12.png" }
     ],
     milktea: [
-        { id: 13, name: "Trà Sữa Boba", price: 45000, image: "images/13.png" },
-        { id: 14, name: "Trà Sữa Matcha", price: 47000, image: "images/14.png" }
+        { id: 13, name: "Trà Sữa Boba", price: 45000, image: "bomilktea.store/order/images/13.png" },
+        { id: 14, name: "Trà Sữa Matcha", price: 47000, image: "bomilktea.store/order/images/14.png" }
     ],
     lemontea: [
-        { id: 15, name: "Trà Chanh", price: 35000, image: "images/15.png" }
+        { id: 15, name: "Trà Chanh", price: 35000, image: "bomilktea.store/order/images/15.png" }
     ],
     snacks: [
-        { id: 16, name: "Bánh Mì", price: 15000, image: "images/16.png" },
-        { id: 17, name: "Khoai Tây Chiên", price: 20000, image: "images/17.png" }
+        { id: 16, name: "Bánh Mì", price: 15000, image: "bomilktea.store/order/images/16.png" },
+        { id: 17, name: "Khoai Tây Chiên", price: 20000, image: "bomilktea.store/order/images/17.png" }
     ]
 };
 
